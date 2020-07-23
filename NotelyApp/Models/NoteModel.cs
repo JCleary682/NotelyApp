c00a73d2ff1a633f84369e1a8bd398bd3c0e73b5
James Cleary <jcleary05@qub.ac.uk>
2020-07-23 21:02:15 +0100

    Notely: Create Note Model

---
+++ NotelyApp/Models/NoteModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NotelyApp.Models
+{
+    public class NoteModel
+    {
+        //Variables
+        public Guid Id { get; set; }
+        public string Subject { get; set; }
+        public string Detail { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
